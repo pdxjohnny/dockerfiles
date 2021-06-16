406f0b94838f7dcd1792c394061a2ee18c4f7487
John Andersen <johnandersenpdx@gmail.com>
2021-06-16 15:57:01 -0700

    mutt: TLS compat hacks
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

--- mutt/entrypoint.sh
+++ mutt/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+# Set environment variable DEBUG=1 for debugging to /home/user/log
 # Usage:
 # docker run --name=mutt -ti -v $HOME/Documents/mutt:/home/user/muttv -e KUSER=$(keyring get intel idsid) -e KDOMAIN=AMR.CORP.INTEL.COM -e KPASSWORD=$(keyring get intel password) mutt
 
@@ -11,8 +12,15 @@ for i in $(ls -a ~/muttv | grep -v -w '..' | grep -v -w '.'); do
    ln -vs ~/muttv/$i ~/$i;
 done
 
-echo -e '[overrides]\ndefault-priority-string = NORMAL:-VERS-ALL:+VERS-TLS1.1:+VERS-TLS1.0:+VERS-SSL3.0:%COMPAT' > gnutls.ini
+echo -e '[overrides]\ndefault-priority-string = NORMAL:+VERS-ALL:%COMPAT' > gnutls.ini
 
-printf "${KPASSWORD}" | kinit "${KUSER}@${KDOMAIN}"
+echo "${KPASSWORD}" | kinit "${KUSER}@${KDOMAIN}"
 
-GNUTLS_SYSTEM_PRIORITY_FAIL_ON_INVALID=1 GNUTLS_SYSTEM_PRIORITY_FILE=gnutls.ini GNUTLS_DEBUG_LEVEL=6 mutt 2>log
+if [[ "x${DEBUG}" != "x" ]]; then
+  export GNUTLS_SYSTEM_PRIORITY_FAIL_ON_INVALID=1
+  export GNUTLS_SYSTEM_PRIORITY_FILE=gnutls.ini
+  export GNUTLS_DEBUG_LEVEL=6
+  export DEBUG="-v"
+fi
+
+mutt $DEBUG 2>log
